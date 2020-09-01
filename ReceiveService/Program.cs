3385e50d428c57b0f8ed8ff55224b4c6583279f1
Lê Thanh Tuấn <tuanitpro@gmail.com>
2020-09-01 20:12:06 +0700

    Topshelf Service

---
+++ ReceiveService/Program.cs
@@ -0,0 +1,30 @@
+﻿using Topshelf;
+
+namespace ReceiveService
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.EnableServiceRecovery(rc =>
+                {
+                    rc.RestartService(1);
+                });
+                x.Service<RabbitMQService>(s =>
+                {
+                    s.ConstructUsing(name => new RabbitMQService());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.StartAutomatically();
+                x.SetServiceName("RabbitMQService");
+                x.SetDisplayName("RabbitMQService Display Name");
+                x.SetDescription("RabbitMQService Description");
+            });
+        }
+    }
+}
---
+++ ReceiveService/RabbitMQService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveService
+{
+    public class RabbitMQService
+    {
+        public async Task Start()
+        {
+            RabbitMQWorker();
+        }
+
+        public async Task Stop()
+        {
+        }
+
+        private void RabbitMQWorker()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            factory.AutomaticRecoveryEnabled = true;
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
